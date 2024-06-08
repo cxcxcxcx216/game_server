b46d3632ffa87894ca8dae604535dea934097e18
cx <473427618@qq.com>
2024-06-08 20:04:04 +0800

    测试

--- game-logic/src/main/java/com/game/cache/BagManager.java
+++ game-logic/src/main/java/com/game/cache/BagManager.java
@@ -1,4 +1,11 @@
 package com.game.cache;
 
+
 public class BagManager {
+
+    private static final BagManager INSTANCE = new BagManager();
+
+    public static BagManager getInstance(){
+        return INSTANCE;
+    }
 }
