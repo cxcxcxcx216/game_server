5a6de8958b51a499674c9bd0ebc8df93a95b861e
cx <473427618@qq.com>
2024-06-08 11:15:09 +0800

    测试

---
+++ game-frame/src/main/java/com/game/util/PackageScanner.java
@@ -0,0 +1,21 @@
+package com.game.util;
+
+
+import org.reflections.Reflections;
+
+import java.util.Set;
+
+public class PackageScanner {
+
+
+    //扫描action
+    public static Set<Class<?>> scan(String packageName,Class clazz){
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(clazz);
+        return classes;
+
+    }
+
+
+
+}
--- game-logic/src/main/java/com/game/handler/HandlerFactory.java
+++ game-logic/src/main/java/com/game/handler/HandlerFactory.java
@@ -1,9 +1,12 @@
 package com.game.handler;
 
+import com.game.annotation.Action;
+import com.game.util.PackageScanner;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 
 @Slf4j
@@ -12,6 +15,29 @@ public class HandlerFactory {
     private static final HandlerFactory INSTANCE = new HandlerFactory();
 
     private static final Map<Integer,BaseHandler> HANDLER_MAP = new HashMap<>();
+
+    public static HandlerFactory getInstance(){
+        return INSTANCE;
+    }
+
+    static {
+        init();
+    }
+    public static void init(){
+        Set<Class<?>> classes = PackageScanner.scan("com.game", BaseHandler.class);
+        for (Class<?> clazz : classes) {
+            Action action = clazz.getAnnotation(Action.class);
+            int msgId = action.msgId();
+
+            try {
+                BaseHandler handler = (BaseHandler) clazz.newInstance();
+                HANDLER_MAP.put(msgId, handler);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
 
     public static BaseHandler getHandler(int type){
         BaseHandler handler = HANDLER_MAP.get(type);
